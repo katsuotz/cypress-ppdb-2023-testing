929cdc956553c75323e0d6083ef0bf1a3a11aeef
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-02-07 12:09:59 +0700

    fix auth testing

--- cypress/e2e/1-auth/auth.cy.js
+++ cypress/e2e/1-auth/auth.cy.js
@@ -1,5 +1,24 @@
 describe('auth', () => {
-  beforeEach(() => {
+  it('check logged in user', () => {
+    cy.visit('https://daftar-ppdb2023.deltamaya.tech/')
+    cy.get('input[name=Username]')
+      .type('20219271')
+    cy.get('input[name=Password]')
+      .type('12345678')
+    cy.get('button')
+      .click().then( () => {
+      cy.saveLocalStorage('user')
+
+      cy.wait(1000)
+
+      const user = JSON.parse(localStorage.getItem('user')) || {}
+      expect(user._id).to.eq('628644b379f2ff00479852f7')
+      expect(user.name).to.eq('Admin SMAN 1 BANDUNG')
+      expect(user.role).to.eq('admin-highschool')
+    })
+  })
+
+  it('logout user', () => {
     cy.visit('https://daftar-ppdb2023.deltamaya.tech/')
     cy.get('input[name=Username]')
       .type('20219271')
@@ -8,32 +27,19 @@ describe('auth', () => {
     cy.get('button')
       .click()
     cy.saveLocalStorage('user')
-  })
-
-  it('check logged in user', () => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    cy.log(user)
-    expect(user._id).to.eq('628644b379f2ff00479852f7')
-    expect(user.name).to.eq('Admin SMAN 1 BANDUNG')
-    expect(user.role).to.eq('admin-highschool')
-  })
-
-  it('logout user', () => {
-    cy.visit('https://daftar-ppdb2023.deltamaya.tech/')
-    cy.get('[aria-haspopup=menu]').click()
-    cy.contains('Logout').click()
-
-    const user = localStorage.getItem('user')
-    expect(user, undefined)
-  })
-
-  it('check wrong pasword user login', () => {
-    cy.visit('https://daftar-ppdb2023.deltamaya.tech/')
-    cy.get('[aria-haspopup=menu]').click()
-    cy.contains('Logout').click()
 
     cy.wait(1000)
 
+    cy.get('[aria-haspopup=menu]').click()
+    cy.contains('Logout').click().then(() => {
+      cy.wait(1000)
+
+      const user = localStorage.getItem('user')
+      expect(user).to.be.null
+    })
+  })
+
+  it('check wrong pasword user login', () => {
     cy.visit('https://daftar-ppdb2023.deltamaya.tech/')
     cy.get('input[name=Username]')
       .type('20219271')
